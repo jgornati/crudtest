a0d7703612cb53c445aabfd720603022d7f41010
jgornati <joelgornati@gmail.com>
2015-04-11 19:17:43 -0300

    hecho hasta el alta

---
+++ routes/main.js
@@ -0,0 +1,29 @@
+var app = module.parent.exports.app;
+//agrega el modelo persona
+var Persons = require('../models/persons.js');
+
+
+app.get('/list', function(req, res) {
+  Persons.find({}, function(err, docs) {
+    //res.json(docs);
+    res.render('list', { title: 'List', persons: docs });
+  });
+});
+
+app.get('/p/new', function(req, res){
+  res.render('new', {title: 'New'});
+});
+
+
+app.post('/p/new', function(req, res){
+  //res.render('new',{title: 'New'});
+  console.log(req.body);
+  var p = new Persons({name: req.body.name, age: req.body.age});
+  p.save(function(err, doc){
+    if(!err){
+      res.redirect('/list');
+    }else{
+      res.end(err);
+    }
+  });
+});
